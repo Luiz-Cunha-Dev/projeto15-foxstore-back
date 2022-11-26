555f7de7de168b0f746c9977603995dff8396360
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-26 18:13:45 -0300

    Update  checkout.controller

--- src/controllers/checkout.controller.js
+++ src/controllers/checkout.controller.js
@@ -25,7 +25,7 @@ async function CheckoutController(req, res) {
                 }
             }
         await db.collection("orders").insertOne({ token, total, cart });
-        await db.collection("cart").deleteMany({});
+        await db.collection("cart").deleteMany({ token });
         res.sendStatus(200).send("Order placed successfully");
     }
     catch (err) {
@@ -39,7 +39,7 @@ async function GetCheckoutController(req, res) {
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
     try {
-        const orderList = await db.collection("orders").findOne({ token }).toArray();
+        const orderList = await db.collection("orders").findOne({ token });
         res.status(200).send(orderList);
     } catch (err) {
         console.log(err);
