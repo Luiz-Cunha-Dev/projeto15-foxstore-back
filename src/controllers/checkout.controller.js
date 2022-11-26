80f3275dd19b6318865ad77c4ec1fe9e627c42be
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-26 11:41:36 -0300

    Checkout --> OK
    checkout router --> OK
    checkout controller --> OK

--- src/controllers/checkout.controller.js
+++ src/controllers/checkout.controller.js
@@ -12,17 +12,12 @@ async function CheckoutController(req, res) {
         if (!cart) {
             res.status(404).send("your cart is empty");
         }
-        //comparar se o produto existe no carrinho e se existe, subtrai a quantidade do produto no carrinho com a quantidade do produto no banco de dados
        for(let i = 0; i < cart.length; i++){
-        res.send("Checkout 4");
               for(let j = 0; j < product.length; j++){
                 let productInventory = product[j].inventory;
                     if(cart[i].name === product[j].name){
                         productInventory -= cart[i].qtde;
-                        if(productInventory < 0){
-                            res.status(404).send("insufficient inventory");
-                        }
-                        else{
+                        if(productInventory > 0){
                             await db.collection("products").updateOne({name: product[j].name}, {$set: {inventory: productInventory}});
                             total += cart[i].value * cart[i].qtde;
                         }
@@ -31,6 +26,7 @@ async function CheckoutController(req, res) {
             }
         await db.collection("orders").insertOne({ token, total, cart });
         await db.collection("cart").deleteMany({});
+        res.sendStatus(200).send("Order placed successfully");
     }
     catch (err) {
         console.log(err)
@@ -43,17 +39,12 @@ async function GetCheckoutController(req, res) {
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
     try {
-        const orderList = await db.collection("orders").find({ token }).toArray();
+        const orderList = await db.collection("orders").findOne({ token }).toArray();
         res.status(200).send(orderList);
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
     }
-}
-
-
-function PrintInScreen(req, res) {
-    res.send("Print in screen");
 }
 
 export { CheckoutController, GetCheckoutController, PrintInScreen };
